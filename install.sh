3d88dd2f7d3703a90418df4f3cc4dfff93aa4963
Christian Hellsten <christian.hellsten@aktagon.com>
2025-01-30 12:34:36 +0200

    :bikini:

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ brew install zsh-autosuggestions
 
 # Update .zshrc to include plugins
 echo "Configuring plugins in .zshrc..."
-PLUGINS_LINE="plugins=(git autojump ssh-agent gpg-agent)"
+PLUGINS_LINE="plugins=(git dotenv autojump ssh-agent gpg-agent)"
 
 # Backup existing .zshrc
 if [ -f "$HOME/.zshrc" ]; then
